3e60920f8ebe199631019e9f5889eee83a0eb6d6
Tuğçe Ergün <tugceergun000@gmail.com>
2025-02-10 01:43:34 +0300

    nativewind added

--- coffee-shop-app/babel.config.js
+++ coffee-shop-app/babel.config.js
@@ -3,16 +3,20 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      ["module:react-native-dotenv", {
-        "envName": "APP_ENV",
-        "moduleName": "@env",
-        "path": ".env",
-        "blocklist": null,
-        "allowlist": null,
-        "safe": false,
-        "allowUndefined": true,
-        "verbose": false
-      }]
+      ["nativewind/babel"],
+      [
+        "module:react-native-dotenv",
+        {
+          envName: "APP_ENV",
+          moduleName: "@env",
+          path: ".env",
+          blocklist: null,
+          allowlist: null,
+          safe: false,
+          allowUndefined: true,
+          verbose: false,
+        },
+      ],
     ],
   };
 };
