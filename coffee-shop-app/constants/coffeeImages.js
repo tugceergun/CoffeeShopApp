f7ffe6532987885833370918a8684bb43d5bda97
Tuğçe Ergün <tugceergun000@gmail.com>
2025-02-10 20:57:34 +0300

    coffees listed on home screen

---
+++ coffee-shop-app/constants/coffeeImages.js
@@ -0,0 +1,11 @@
+
+export const coffeeImages = {
+    "Latte": require("../assets/coffees/latte.jpg"),
+    "Espresso": require("../assets/coffees/espresso.jpg"),
+    "Cappuccino": require("../assets/coffees/cappuccino.jpg"),
+    "Americano": require("../assets/coffees/americano.jpg"),
+    "Mocha": require("../assets/coffees/mocha.jpg"),
+    "Macchiato": require("../assets/coffees/macchiato.jpg"),
+    "Flat White": require("../assets/coffees/flatwhite.jpg"),
+    "Caramel Latte": require("../assets/coffees/caramelLatte.jpg"),
+};
--- coffee-shop-app/screens/HomeScreen.js
+++ coffee-shop-app/screens/HomeScreen.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, FlatList, Button } from "react-native";
+import { View, Text, FlatList, Button, Image, Pressable } from "react-native";
 import { api } from "../services/api";
+import { coffeeImages } from "../constants/coffeeImages";
 
 const HomeScreen = ({ navigation }) => {
-
     const [coffees, setCoffes] = useState([]);
 
     useEffect(() => {
@@ -13,19 +13,40 @@ const HomeScreen = ({ navigation }) => {
     }, []);
 
     return (
-        <View>
-        <Text>☕ Coffee List</Text>
-        <FlatList
-          data={coffees}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({ item }) => (
-            <View>
-              <Text>{item.name}</Text>
-              <Button title="Add to Cart" onPress={() => console.log("Added to cart", item.name)} />
-            </View>
-          )}
-        />
-      </View>
+        <View className="flex-1 p-4 bg-white">
+            <Text className="text-2xl font-bold mb-4">☕ Coffee List</Text>
+            <FlatList
+                data={coffees}
+                keyExtractor={(item) => item.id.toString()}
+                showsVerticalScrollIndicator={false}
+                overScrollMode="never"
+                numColumns={2}
+                columnWrapperStyle={{ justifyContent: "space-between" }}
+                renderItem={({ item }) => (
+
+                    <View className="bg-white p-4 mb-4 rounded-lg shadow-xl shadow-gray-300">
+                        <Image
+                            source={coffeeImages[item.name]}
+                            className="w-32 h-32 rounded-lg mb-2"
+                            resizeMode="cover"
+                        />
+                        <Text className="text-lg font-bold mb-2">{item.name}</Text>
+
+                        <View className="flex-row justify-between items-center">
+                            <Text>${item.price}</Text>
+                            <Pressable
+                                onPress={() => console.log("Added to cart", item.name)}
+                                className="w-8 h-8 bg-amber-900 rounded-full flex items-center justify-center mt-2 self-end relative"
+                            >
+                                <Text className="text-white text-lg">+</Text>
+
+                            </Pressable>
+                        </View>
+                    </View>
+
+                )}
+            />
+        </View>
     )
 };
 
